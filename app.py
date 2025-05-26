fd0f887f5362e788c8c61bb29d246fded8ba2b11
Sakethkp <skallep@gmu.edu>
2025-05-26 14:14:15 -0400

    Create app.py
    creating file in remote

---
+++ app.py
@@ -0,0 +1,14 @@
+# Sample Python Code: Hello, World! and Simple Addition
+
+# Print a greeting message
+print("Hello, World!")
+
+# Define two numbers
+num1 = 10
+num2 = 20
+
+# Calculate the sum
+sum = num1 + num2
+
+# Print the result
+print("The sum of", num1, "and", num2, "is", sum)
